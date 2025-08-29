d1d02fe61fc2c6d0bf19e011430bf7613422bb19
elenawu-lab <elena.wu@ucas-edu.net>
2025-08-29 14:42:42 -0600

    variables notes

---
+++ practices/variables_practice.py
@@ -0,0 +1,14 @@
+# EW 7th Varible Notes
+
+# What is one way to have your program give an output?
+print("Welcome to the Variable Notes!")
+
+#exam of variables
+school = "UCAS"
+single_digit = 4
+
+print(school)
+
+name = input("What is your name?")
+
+print("Hello", name)
