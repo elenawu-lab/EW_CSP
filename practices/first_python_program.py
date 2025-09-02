aca8c206446171b4a8d983b12467596c3429cd78
elenawu-lab <elena.wu@ucas-edu.net>
2025-09-02 14:42:21 -0600

    first program

---
+++ practices/first_python_program.py
@@ -0,0 +1,4 @@
+# E.W 7 First Python Program
+
+username = input("What is your name?")
+print("Hello", username)
---
+++ practices/variable_practice.py
@@ -0,0 +1,23 @@
+# E.W 7 Variable Notes
+
+your_name = ("elena")
+number_between_1_and_10 = 4
+A_number_between_one_hundred_and_one_thousand = 555
+what_you_had_for_breakfast = ("nothing")
+your_favorite_color = ("green")
+the_school_name = ("ucas")
+the_year = 2025
+your_eye_color = ("brown")
+your_age = 14
+your_favorite_subject_in_school = ("math")
+
+print(your_name)
+print(number_between_1_and_10)
+print(A_number_between_one_hundred_and_one_thousand)
+print(what_you_had_for_breakfast)
+print(your_favorite_color)
+print(the_school_name)
+print(the_year)
+print(your_eye_color)
+print(your_age)
+print(your_favorite_subject_in_school)
