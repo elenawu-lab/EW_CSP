e6e9d92411d9ab40adf741d36431f6c3e10a6bd7
elenawu-lab <elena.wu@ucas-edu.net>
2025-09-04 14:41:07 -0600

    Expression Notes

---
+++ practices/expression.py
@@ -0,0 +1,27 @@
+# EW 7th Expressions
+
+name = input("What is your name?\n")
+print("Hello", name)
+
+# Treyson has 12 apples, he has 5 friends he wants to give apples to. How many apples do each friend get?
+apples = 12
+friends = 5
+print("Each friend gets", apples//friends, "apples!")
+
+# Average age of a group of 4 people
+Friend1 = 14
+Friend2 = 19
+Friend3 = 9
+Friend4 = 12
+print("Average age of friends is", (Friend1+Friend2+Friend3+Friend4)/4)
+
+num1 = 12
+num2 = 7
+
+print("Addition(+)", num1+num2)
+print("Subtraction(-)", num1-num2)
+print("Multiplication(*)", num1*num2)
+print("Division(/)", num1/num2)
+print("Exponents(**)", num1**num2)
+print("Integer Division(//)", num1//num2)
+print("Modulo(%)", num1%num2)
