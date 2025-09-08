8e661b54d84ac6171364ce0fe8a03c8cd7bf9a86
elenawu-lab <elena.wu@ucas-edu.net>
2025-09-08 14:41:35 -0600

    new notes

--- Notes/strings.py
+++ Notes/strings.py
@@ -1,3 +1,10 @@
 # EW 7th Strings Notes
 
-print("I did it")
+first_name = input("What is your first name?\n").strip().title()
+last_name = input("What is your last name?\n").strip().title()
+
+sentence = 'The quick brown fox jumps over the lazy dog.'
+
+full_name = first_name + " " + last_name
+
+print("Welcome to my program", full_name + "!")
