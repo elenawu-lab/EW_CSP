4803be3f29a98ec60273fe9c8d5ef8b0db2977ec
elenawu-lab <elena.wu@ucas-edu.net>
2025-09-10 14:28:13 -0600

    finished silly sentences

---
+++ practices/silly_sentences.py
@@ -0,0 +1,8 @@
+# EW 7th Silly Sentences
+
+print("Welcome to Silly Sentences. Answer the questions to get a silly sentence.")
+verb = input("First I need a verb\n").strip().lower()
+verb2 = input("Now I need a second verb\n").strip().lower()
+sound = input("Now tell me a sound\n").strip().lower()
+
+print("As I " + verb + " out the door, I started to " + verb2 + " because I heard a " + sound + ".")
