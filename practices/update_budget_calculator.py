a2290cfeb4d28b01edf3389d841fd0c277f9d355
lobster <elena.wu@ucas-edu.net>
2025-09-17 14:43:49 +0000

    updating budget calculator

---
+++ practices/update_budget_calculator.py
@@ -0,0 +1,31 @@
+# EW 7th Update Budget Calculator
+
+# Rent, Utilities, Groceries, and Transportation
+# Savings is 10% income (income*1)
+# Spending = income - (utilities + rent + groceries + transportation + savings)
+# Percent_rent=rent/income*100
+
+income = int(input("What is your monthly income\n"))
+rent = int(input("What is your monthly rent cost?\n"))
+util = int(input("What is your monthly utilities?\n"))
+groc = int(input("What is your monthly grocery cost?\n"))
+trans = int(input("What is your daily transportation cost?\n"))
+save = income * 0.10 * (income*1)
+spend = income - (util + rent + groc + trans + save)
+
+def what(money):
+    result = input(money)
+    return result
+money = input(f"What is your monthly () cost?\n")
+money(rent)
+money(utilities)
+money(transportation)
+save = income * 0.10 * (income*1)
+spend = income - (util + rent + groc + trans + save)
+
+print("Your rent is $", rent, "and that is", (rent/income*100), " of your income.")
+print("Your utilities is $", util, "and that is", (util/income*100), " of your income")
+print("Your groceries is $", groc, "and that is", (groc/income*100), " of your income")
+print("Your transportation is $", trans, "and that is", (trans/income*100), " of your income")
+print("Your savings is $", save, "and that is", (save/income*100), " of your income")
+print("You have $", spend,"of spending money each month!")
