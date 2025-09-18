b37e274ee7b59466b29211b5cb26589386fd2e46
elenawu-lab <elena.wu@ucas-edu.net>
2025-09-18 14:42:41 -0600

    doing old load

---
+++ practices/old_enough.py
@@ -0,0 +1,23 @@
+# EW 7th Old Enough
+
+# Vote - 18
+# drive - 16
+# Learners permit - 15
+# School - 4
+# 42 - vote
+# 7 - school
+# 17 - drive
+# 3 - not old
+
+age = int(input("How old are you?\n"))
+
+if age >= 18:
+    ("You can vote! You are old...")
+elif age == 16:
+    ("Oh goodie! You are hitting the roads!")
+elif age >= 15 and age <= 15:
+    print("Oh good luck with your drivers permit!")
+elif age > 3 and age < 15:
+    print("You may go to school!")
+elif age <= 3:
+    print("You are too young to do anything. Sorry!")
