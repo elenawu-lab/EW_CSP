6d04b42d048a753bd7db25e21668d08abdd99578
elenawu-lab <elena.wu@ucas-edu.net>
2025-09-23 14:17:49 -0600

    finished family loop and fizz buzz

---
+++ practices/fizzbuzz.py
@@ -0,0 +1,14 @@
+# EW 7th FizzBuzz
+
+x = 1
+
+while x < 51:
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
+    x += 1
---
+++ practices/my_family_loop.py
@@ -0,0 +1,13 @@
+# EW 7th My family Loop
+
+fammems = ["Wildon", "Kristin", "Sierra", "Jason"]
+
+fammems[0] = "Dad"
+fammems[1] = "Mom"
+fammems[2] = "Sister"
+fammems[3] = "Brother"
+
+print(f"Hello {fammems[0]}")
+print(f"Hello {fammems[1]}")
+print(f"Hello {fammems[2]}")
+print(f"Hello {fammems[3]}")
