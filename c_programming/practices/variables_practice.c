47915a91164066fdf57a8236cf7ef34e45ec1480
lobster <elena.wu@ucas-edu.net>
2025-10-09 02:44:09 +0000

    finished variables practice

---
+++ c_programming/practices/variables_practice.c
@@ -0,0 +1,26 @@
+// EW 7th Variables Practice
+#include <stdio.h>
+
+int main(void){
+    char name[] = "Elena";
+    int num = 64;
+    float gpa = 4.0;
+    char breakfast[] = "bagel";
+    char color[] = "green";
+    char school[] = "UCAS";
+    int year = 2025;
+    int age = 15;
+    char subject[] = "PE";
+
+    printf("Name: %s\n", name);
+    printf("Favorite Number: %d\n", num);
+    printf("GPA: %f\n", gpa);
+    printf("Breakfast: %s\n", breakfast);
+    printf("Color: %s\n", color);
+    printf("School: %s\n", school);
+    printf("Year: %d\n", year);
+    printf("Age: %d\n", age);
+    printf("Subject: %s\n", subject);
+    
+    return 0;
+}
