0f04c40636dac06d68be5e7b1856a58afab15030
lobster <elena.wu@ucas-edu.net>
2025-10-10 21:30:50 +0000

    finished silly sentences

--- c_programming/practices/silly_sentences.c
+++ c_programming/practices/silly_sentences.c
@@ -3,19 +3,19 @@
 #include <string.h>
 
 int main(void){
-    printf("Welcome to Silly Sentences. Answer the questions to get a silly sentence.");
+    printf("Welcome to Silly Sentences. Answer the questions to get a silly sentence.\n");
     
     char verb[30];
     char verbs[30];
     char sound[30];
 
-    printf("First I need a verb\n");
+    printf("First I need a verb.\n");
     scanf("%s", &verb);
 
-    printf("Now I need a second verb\n");
+    printf("Now I need a second verb.\n");
     scanf("%s", &verbs);
 
-    printf("Now tell me a sound\n");
+    printf("Now tell me a sound.\n");
     scanf("%s", &sound);
 
     printf("As I %s out the door, I started to %s because I heard a %s.\n", verb, verbs, sound);
