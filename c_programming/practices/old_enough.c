31b99128d49facbe85ccdd31f811ade3f23a78ea
lobster <elena.wu@ucas-edu.net>
2025-10-11 04:29:52 +0000

    finished old enough

--- c_programming/practices/old_enough.c
+++ c_programming/practices/old_enough.c
@@ -3,18 +3,21 @@
 #include <string.h>
 
 int main(void){
-    int age[3];
+    int age;
     printf("How old are you?\n");
-    scanf("%d", age);
+    scanf("%d", &age);
 
     if(age >= 18){
-        printf("You can vote! You are old...\n")
-    }elif age >= 16:
-        print("Oh goodie! You are hitting the roads!")
-    elif age >= 15 and age <= 15:
-        print("Oh good luck with your drivers permit!")
-    elif age > 3 and age < 15:
-        print("You may go to school!")
-    else:
-        print("You are too young to do enything. Haha L")
+        printf("You can vote! You are old...\n");
+    }else if(age >= 16){
+        printf("Oh goodie! You are hitting the roads!\n");
+    }else if(age >= 15 && age <= 15){
+        printf("Oh good luck with your drivers permit!\n");
+    }else if(age > 3 && age < 15){
+        printf("You may go to school!\n");
+    }else{
+        printf("You are too young to do enything. Haha L\n");
+    }
+
+    return 0;
 }
