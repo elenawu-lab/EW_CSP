f71b33111e78cbcb02f12297e745fc13b7e5db13
elenawu <elena.wu@ucas-edu.net>
2025-10-13 14:42:00 -0600

    doing fizzbuzz

---
+++ c_programming/practices/fizzbuzz.c
@@ -0,0 +1,20 @@
+// EW 7th FizzBuzz
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    while(int x = 0; x < 51; x++){
+        if(x = 0; x < 0 && x % 5 == 0){
+            printf("FizzBuzz");
+        }else if(x % 3 == 0){
+            printf("Fizz");
+        }else if(x % 5 == 0){
+            printf("Buzz");
+        }else{
+            printf(x);
+        }
+    }
+        x += 1;
+
+    return 0;
+}
