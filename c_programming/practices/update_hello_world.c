03ed9aca80df8e07e85dc14c9b9eab62e0e4b776
elenawu <elena.wu@ucas-edu.net>
2025-10-15 14:13:44 -0600

    finsihed update hello world

---
+++ c_programming/practices/update_hello_world.c
@@ -0,0 +1,17 @@
+// EW 7th Update Hello World
+#include <stdio.h>
+#include <string.h>
+
+void whop(char* name) {
+    printf("Hello %s\n", name);
+}
+
+int main(void){
+    whop("Elena");
+    whop("Sierra");
+    whop("Jason");
+    whop("Wildon");
+    whop("Gary");
+
+    return 0;
+}
